7374896b4eb9971d7a25bccc213854b09b5cfcfa
pbojinov <petarbojinov@gmail.com>
2014-05-20 12:09:01 -0700

    before lunch commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,8 +2,20 @@ var app = angular.module('weatherApp', []);
 
 app.controller('HomeController', ['$scope', '$http',
     function($scope, $http) {
+
+        var weatherUrl = 'http://api.openweathermap.org/data/2.5/' +
+            'forecast/daily?mode=json' +
+            '&units=imperial&cnt=1' +
+            '&callback=JSON_CALLBACK' +
+            '&q=';
+
         $scope.fetchData = function() {
             console.log('fetchData called', $scope.city);
+
+            $http({
+            	method: 'JSONP',
+            	url: weatherUrl + $scope.city
+            });
         }
     }
 ]);
