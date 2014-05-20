101ddb68d1042812c062e1629297ce6ad62a254d
pbojinov <petarbojinov@gmail.com>
2014-05-20 14:56:52 -0700

    refactor yelp http to service as well

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,13 @@
 var app = angular.module('weatherApp', []);
 
-app.service('WeatherService', ['$http',
-    function() {
+app.service('WeatherService', ['$http', '$q',
+
+    function($http, $q) {
 
         var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast/daily?';
 
         this.getWeatherFor = function(city) {
-            $http({
+            return $http({
                 method: 'JSONP',
                 url: weatherUrl,
                 params: {
@@ -21,48 +22,71 @@ app.service('WeatherService', ['$http',
                 return data.data;
             }).
             catch (function(err) {
-                console.error('error in weather fetch ', err); 
+                console.error('error in weather fetch ', err);
+                $q.reject({
+                    error: err
+                });
             });
-        };
+        }
     }
 ]);
 
-app.controller('HomeController', ['$scope', '$http', 'WeatherService',
+app.service('YelpService', ['$http', '$q',
 
-    function($scope, $http, WeatherService) {
-
-        $scope.city = 'San Francisco, CA';
-
-        $scope.fetchData = function() {
-            $scope.getWeatherData();
-            $scope.getYelpData();
-        }
-
-        $scope.getWeatherData = function() {
-            console.log('fetchData called', $scope.city);
-            WeatherService.getWeatherFor($scope.city).then(function(weather) {
-                $scope.weather = weather;
-            });
-        }
+    function($http, $q) {
 
         var yelpUrl = 'http://api.yelp.com/business_review_search',
             yelpKey = 'ZS-n2aias1RlYWGelWbogw';
 
-        $scope.getYelpData = function() {
-            $http({
+        this.getLocationsFor = function(city) {
+            return $http({
                 method: 'JSONP',
                 url: yelpUrl,
                 params: {
                     callback: 'JSON_CALLBACK',
                     ywsid: yelpKey,
-                    location: $scope.city
+                    location: city
                 }
             }).then(function(data) {
                 console.log('data', data.data);
-                $scope.nearby = data.data;
+                return data.data;
             }).
             catch (function(err) {
                 console.error('error in yelp fetch', err);
+                $q.reject({
+                    error: err
+                });
+            });
+        }
+    }
+]);
+
+app.controller('HomeController', ['$scope', '$http', 'WeatherService', 'YelpService',
+
+    function($scope, $http, WeatherService, YelpService) {
+
+        $scope.city = 'San Francisco, CA';
+
+        $scope.fetchData = function() {
+            console.log('fetchData called', $scope.city);
+            $scope.getWeatherData();
+            $scope.getYelpData();
+        }
+
+        $scope.getWeatherData = function() {
+            WeatherService.getWeatherFor($scope.city).then(function(weather) {
+                $scope.weather = weather;
+            }).
+            catch (function(err) {
+                $scope.error = err;
+            });
+        }
+
+        $scope.getYelpData = function() {
+            YelpService.getLocationsFor($scope.city).then(function(nearby) {
+                $scope.nearby = nearby;
+            }).
+            catch (function(err) {
                 $scope.error = err;
             });
         }
