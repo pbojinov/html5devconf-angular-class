8dc0c24553e52b071f343d6683b2d4a59103312c
pbojinov <petarbojinov@gmail.com>
2014-05-20 12:02:04 -0700

    get ready to setup weather search

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,9 @@
 var app = angular.module('weatherApp', []);
 
-app.controller('HomeController', function($scope) {
-	$scope.name = 'Petar';
-	$scope.sayHello = function() {
-		$scope.name = 'Someone else'
-		alert('Hello ' + $scope.name);
-	}
-});
+app.controller('HomeController', ['$scope', '$http',
+    function($scope, $http) {
+        $scope.fetchData = function() {
+            console.log('fetchData called', $scope.city);
+        }
+    }
+]);
